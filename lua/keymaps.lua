4bca67935de039a161b258b3f5f58c4508a981a8
haikeyi <4voacdo@gmail.com>
2026-04-05 17:59:24 +0800

    new keymaps with leader key!

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,7 +2,19 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Set <leader>w to save file
-vim.keymap.set('n', '<leader>w', ':w<CR>', {silent = true, desc = 'Save file'})
--- Set <leader>e to toggle nvim-tree
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { silent = true, desc = 'Toggle NvimTree' })
+-- save file
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
+
+vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit current window" })
+
+vim.keymap.set("n", "<leader>Q", ":qa<CR>", { desc = "Quit all" })
+
+-- vim.keymap.set("n", "<leader>wq", ":wqa<CR>", { desc = " Write and quit current window" })
+--
+-- vim.keymap.set("n", "<leader>wQ", ":wqa<CR>", { desc = " Write and quit all" })
+
+-- toggle nvim tree
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true, desc = "Toggle NvimTree" })
+
+-- select all
+vim.keymap.set({ "n", "v", "i" }, "<leader>a", "ggVG", { desc = "Select all" })
